0842dea33e7b877d1d5599e663f1f2c2eebf8e2b
e.babitskiy <e.babitskiy@datareon.ru>
2026-04-20 23:28:49 +0300

    Implement server and client

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,1 +1,17 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Dummy;
+using Grpc.Core;
+
+const string Target = "127.0.0.1:50051";
+
+Channel channel = new(Target, ChannelCredentials.Insecure);
+
+channel.ConnectAsync().ContinueWith((task) =>
+{
+    if (task.Status == TaskStatus.RanToCompletion)
+        Console.WriteLine("The client connected successfully");
+});
+
+var client = new DummyService.DummyServiceClient(channel);
+
+channel.ShutdownAsync().WaitAsync();
+Console.ReadKey();
--- Server/Program.cs
+++ Server/Program.cs
@@ -1,1 +1,28 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Grpc.Core;
+
+const int Port = 50051;
+
+Server server = null;
+
+try
+{
+	server = new Server()
+	{
+		Ports = { new ServerPort("localhost", Port, ServerCredentials.Insecure) }
+	};
+
+	server.Start();
+
+    Console.WriteLine("The server is listening on the port " + Port);
+	Console.ReadKey();
+}
+catch (IOException e)
+{
+    Console.WriteLine("The server failed to start " + e.Message);
+    throw;
+}
+finally
+{
+	if (server is not null)
+		await server.ShutdownAsync().wai;
+}
